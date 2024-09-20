e2c91794b6f4d453187f6a934bfd4b7e6b006728
Highwe2hell <take.the.red@outlook.com>
2024-09-20 21:34:30 +0800

    1&9

---
+++ src/_1TwoSum.java
@@ -0,0 +1,25 @@
+// 给定一个整数数组 nums 和一个整数目标值 target，请你在该数组中找出和为目标值 target 的那两个整数，并返回它们的数组下标。
+// 你可以假设每种输入只会对应一个答案，并且你不能使用两次相同的元素。
+// 你可以按任意顺序返回答案。
+public class _1TwoSum {
+    public static void main(String[] args) {
+        int[] nums = {3, 2, 4};
+        int target = 6;
+        int[] res = twoSum(nums, target);
+        System.out.println(res[0] + " " + res[1]);
+    }
+
+    public static int[] twoSum(int[] nums, int target) {
+        int[] res = new int[2];
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = i + 1; j < nums.length; j++) {
+                if (nums[i] + nums[j] == target) {
+                    res[0] = i;
+                    res[1] = j;
+                    break;
+                }
+            }
+        }
+        return res;
+    }
+}
